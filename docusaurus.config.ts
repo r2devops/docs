c2042b2f50363304cf675162ac06334ee64be04b
Totara-thib <totara-thib@gmail.com>
2024-05-31 16:40:18 +0200

    ci: adjust github deploy workflows

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: 'img/logo-documentation.svg',
 
   // Set the production url of your site here
-  url: 'https://docs.r2devops.io/',
+  url: 'https://docs.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -18,6 +18,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'r2devops', // Usually your GitHub org/user name.
   projectName: 'documentation', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
